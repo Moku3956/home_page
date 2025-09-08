a2f1069ebe29d63c53b648ff59e2e691855a1330
moku <moku3956@icloud.com>
2025-09-08 23:29:36 +0900

    refactor: update layout and add images

--- start_page/views.py
+++ start_page/views.py
@@ -40,5 +40,6 @@ class AboutTeamView(View):
     def get(self, request):
         return render(request, 'start_page/about_team.html', {
             'team_name': '野路コレクション',
-            'team_description': '野路コレクションは、立命館大学びわこ・くさつキャンパスのサッカーサークルです。'
+            'team_description': 
+                '野路コレクションは、立命館大学びわこ・くさつキャンパスのサッカーサークルです。みたいな感じで文章入れれます。レイアウトも、詳しく指摘していただければ自由に変更できます。'
         })
